90730d7ae682537a59dd731ca1cbb54f50276f32
Farhan <farhanahad3@gmail.com>
2022-03-10 12:22:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import OrderReview from './components/OrderReview/OrderReview';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+    <Header/>
+      <Switch>
+        <Route exact path='/'>
+          <Shop/>
+        </Route>
+        <Route  path='/shop'>
+          <Shop/>
+        </Route>
+        <Route  path='/review'>
+          <OrderReview/>
+        </Route>
+        <Route  path='/inventory'>
+          <Inventory/>
+        </Route>
+        <Route path='/placeorder'>
+          <PlaceOrder/>
+        </Route>
+        <Route path="*">
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
